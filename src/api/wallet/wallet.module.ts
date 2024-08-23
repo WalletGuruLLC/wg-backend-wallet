6fb723cbf687a3b00d7b7c00650f62e9ffb96f9a
Cristian <christian.dulcey@gmail.com>
2024-08-23 03:50:18 -0500

    add feature verify jwt in the endpoint of list wallets

--- src/api/wallet/wallet.module.ts
+++ src/api/wallet/wallet.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { WalletController } from './controller/wallet.controller';
 import { WalletService } from './service/wallet.service';
 import { ConfigModule } from '@nestjs/config';
+import {VerifyService} from "../../verify/verify/verify.service";
+import {VerifyModule} from "../../verify/verify.module";
 
 @Module({
-	imports: [ConfigModule],
+	imports: [VerifyModule],
 	controllers: [WalletController],
 	providers: [WalletService],
 })
---
+++ src/verify/verify.module.ts
@@ -0,0 +1,16 @@
+import {Module} from '@nestjs/common';
+import {VerifyService} from './verify/verify.service';
+import {ConfigModule} from "@nestjs/config";
+import envs from "./envs";
+
+@Module({
+    imports: [ConfigModule.forRoot(
+        {
+            load: [envs],
+        }
+    )],
+    providers: [VerifyService],
+    exports: [VerifyService],
+})
+export class VerifyModule {
+}
