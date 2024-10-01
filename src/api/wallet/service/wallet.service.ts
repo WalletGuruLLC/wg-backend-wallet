f6f6beff627425b3c4e3122c94927d0569c0d1d9
Thalles Stakonski <thalles.dev.96@gmail.com>
2024-10-01 19:37:31 -0300

    Fixed the response object on the endpoint api/v1/wallets-rafiki/address

--- src/api/wallet/service/wallet.service.ts
+++ src/api/wallet/service/wallet.service.ts
@@ -449,7 +449,7 @@ export class WalletService {
 			walletType: 'Native',
 			walletAddress: createRafikiWalletAddressInput.walletAddress,
 			rafikiId:
-			createdRafikiWalletAddress.createWalletAddress?.walletAddress?.id,
+				createdRafikiWalletAddress.createWalletAddress?.walletAddress?.id,
 			userId,
 		};
 		if (userInfo?.data?.first) {
@@ -468,8 +468,20 @@ export class WalletService {
 			wallet.rafikiId,
 			wallet.userId
 		);
+		const walletInfo = await this.graphqlService.listWalletInfo(
+			wallet.rafikiId
+		);
+		if (walletCreated.rafikiId) {
+			delete walletCreated.rafikiId;
+		}
 		await this.updateKeys(walletCreated?.id, pairs, keyId);
-		return walletCreated;
+
+		return {
+			walletDb: walletCreated,
+			walletAsset: walletInfo.data.walletAddress.asset,
+			balance: 0,
+			reserved: 0,
+		};
 	}
 
 	async createServiceProviderWalletAddress(
@@ -559,7 +571,7 @@ export class WalletService {
 			walletType: 'Native',
 			walletAddress: createRafikiWalletAddressInput.walletAddress,
 			rafikiId:
-			createdRafikiWalletAddress.createWalletAddress?.walletAddress?.id,
+				createdRafikiWalletAddress.createWalletAddress?.walletAddress?.id,
 			providerId: createServiceProviderWalletAddressDto.providerId,
 		};
 		const walletCreated = await this.create(
