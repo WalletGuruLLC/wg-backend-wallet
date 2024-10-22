33173b93f81b71a8b80ba84b050086683815245e
danielordonezg <danielale.gomez@correo.usa.edu.co>
2024-10-22 11:43:35 -0500

    total cost

--- src/utils/helpers/calcularTotalTransactionPlat.ts
+++ src/utils/helpers/calcularTotalTransactionPlat.ts
@@ -1,4 +1,4 @@
 export function calcularTotalCosto(base, comision, costo, porcentaje) {
 	const serviceProviderCost = costo * (porcentaje / 100) + comision + base;
-	return serviceProviderCost;
+	return Math.round(serviceProviderCost * 100) / 100;
 }
