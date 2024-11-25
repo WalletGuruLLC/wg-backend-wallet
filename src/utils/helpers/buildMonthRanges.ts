88103ec2e51a4945f7c84c55ebf182eb3604f166
Elvin Javier <elvin.javier@scrummers.co>
2024-11-25 18:27:16 -0400

    Fixing enum

--- src/utils/helpers/buildMonthRanges.ts
+++ src/utils/helpers/buildMonthRanges.ts
@@ -4,11 +4,23 @@ export function getDateRangeForMonthEnum(month: Month) {
 	const now = new Date();
 
 	const startDate = new Date(
-		Date.UTC(now.getUTCFullYear(), month - 1, 1, 0, 0, 0, 0)
+		now.getUTCFullYear(),
+		month - 1,
+		1,
+		0,
+		0,
+		0,
+		0
 	).getTime();
 
 	const endDate = new Date(
-		Date.UTC(now.getUTCFullYear(), month, 0, 23, 59, 59, 999)
+		now.getUTCFullYear(),
+		month,
+		0,
+		23,
+		59,
+		59,
+		999
 	).getTime();
 
 	return { startDate, endDate };
