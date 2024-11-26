26aab0e9ce7c000c5a9259621de5cca43512f34b
Elvin Javier <elvin.javier@scrummers.co>
2024-11-26 16:44:04 -0400

    Linting

--- src/api/wallet/service/wallet.service.ts
+++ src/api/wallet/service/wallet.service.ts
@@ -2495,11 +2495,11 @@ export class WalletService {
 			},
 		};
 
-		console.log('Completed transactions params:',params)
+		console.log('Completed transactions params:', params);
 
 		try {
-			const result = await docClient.query(params).promise()
-			console.log('Completed transactions params:',result?.Items)
+			const result = await docClient.query(params).promise();
+			console.log('Completed transactions params:', result?.Items);
 			return convertToCamelCase(result.Items);
 		} catch (error) {
 			Sentry.captureException(error);
@@ -2953,8 +2953,6 @@ export class WalletService {
 						startDate.getTime(),
 						endDate.getTime()
 					);
-
-					
 
 					if (transactions?.length) {
 						const paymentParameters = await this.getPaymentsParameters(
